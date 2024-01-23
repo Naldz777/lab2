1bf33c301a2ac832eb8158a4586fde4b0222c086
David Naldo <dcnaldo@apc.edu.ph>
2024-01-23 09:02:05 +0800

    week 8 PHP Lessons

---
+++ week8/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+echo "My unang PHP script HELLO WORLD! <br>";
+echo "hello World <br>";
+echo "hello World <br>";
+echo "hello World <br>";
+?>
+</body>
+</html>
